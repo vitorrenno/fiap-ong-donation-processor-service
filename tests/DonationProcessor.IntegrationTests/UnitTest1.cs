730dee8597237176960775586f75d742f31b639b
Vitor Renno <vitorrenno@outlook.com>
2026-03-14 23:53:56 -0300

    Initial commit

---
+++ tests/DonationProcessor.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DonationProcessor.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/DonationProcessor.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace DonationProcessor.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
