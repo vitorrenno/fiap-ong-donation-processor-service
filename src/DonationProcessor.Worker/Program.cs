f3aaf6bf8ac3811aedeba72e3ca1390242147608
Zezao1224 <gamer12224@yahoo.com.br>
2026-05-21 02:06:39 -0300

    Docker rodando e gerando grafana e prometheus

--- src/DonationProcessor.Worker/Program.cs
+++ src/DonationProcessor.Worker/Program.cs
@@ -2,6 +2,9 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Configuration
+    .AddJsonFile("appsettings.worker.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.worker.json", optional: true);
 
 builder.Services.AddHealthChecks();
 
